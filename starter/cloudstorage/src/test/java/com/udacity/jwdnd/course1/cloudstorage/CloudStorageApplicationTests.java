a0ef30f11e23f6e65e9851ec489ecc7ef7586d00
REVANTHSAI <revanthsai361@gmail.com>
2020-10-02 21:21:07 +0530

    Added unit tests

--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -14,6 +14,7 @@ import javax.xml.transform.Result;
 import java.util.ArrayList;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CloudStorageApplicationTests {
 
 	@LocalServerPort
@@ -39,66 +40,202 @@ class CloudStorageApplicationTests {
 	@AfterEach
 	public void afterEach() {
 		if (this.driver != null) {
-//			driver.quit();
+			driver.quit();
 		}
 	}
 
 	/*Write a test that verifies that an unauthorized user can only access the
 	login and signup pages.*/
 
-//	@Test
-//	public void unauthorizedUserTest(){
-//		driver.get("http://localhost:" + this.port + "/login");
-//		Assertions.assertEquals("Login", driver.getTitle());
-//
-//		driver.get("http://localhost:" + this.port + "/signup");
-//		Assertions.assertEquals("Sign Up", driver.getTitle());
-//
-//		driver.get("http://localhost:" + this.port + "/home");
-//		Assertions.assertEquals("Login",driver.getTitle());
-//
-//		driver.get("http://localhost:" + this.port + "/credentials");
-//		Assertions.assertEquals("Login",driver.getTitle());
-//
-//		driver.get("http://localhost:" + this.port + "/files");
-//		Assertions.assertEquals("Login",driver.getTitle());
-//
-//		driver.get("http://localhost:" + this.port + "/notes");
-//		Assertions.assertEquals("Login",driver.getTitle());
-//
-//	}
+	@Test
+	@Order(1)
+	public void unauthorizedUserTest_1(){
+		driver.get("http://localhost:" + this.port + "/login");
+		Assertions.assertEquals("Login", driver.getTitle());
+
+		driver.get("http://localhost:" + this.port + "/signup");
+		Assertions.assertEquals("Sign Up", driver.getTitle());
+
+		driver.get("http://localhost:" + this.port + "/home");
+		Assertions.assertEquals("Login",driver.getTitle());
+
+		driver.get("http://localhost:" + this.port + "/credentials");
+		Assertions.assertEquals("Login",driver.getTitle());
+
+		driver.get("http://localhost:" + this.port + "/files");
+		Assertions.assertEquals("Login",driver.getTitle());
+
+		driver.get("http://localhost:" + this.port + "/notes");
+		Assertions.assertEquals("Login",driver.getTitle());
+
+	}
 
 	/*Write a test that signs up a new user, logs in, verifies that the home
 	page is accessible, logs out, and verifies that the home page is no longer accessible.*/
+	@Test
+	@Order(2)
+	public void signUpNewUserTest_2(){
+
+
+		driver.get("http://localhost:" + this.port + "/signup");
+		SignUpPage signUpPage = new SignUpPage(driver);
+		signUpPage.signUp(firstName,lastName,userName,password);
+
+		driver.get("http://localhost:" + this.port + "/login");
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(userName,password);
+
+		// Login and check if user is able to access home page
+		Assertions.assertEquals("Home", driver.getTitle());
+
+		driver.get("http://localhost:" + this.port + "/home");
+		HomePage homePage = new HomePage(driver);
+		homePage.logout();
+
+		//Logout and verify that the user is not able to access home page
+		driver.get("http://localhost:" + this.port + "/home");
+		Assertions.assertEquals("Login",driver.getTitle());
+
+	}
+
+
 //	@Test
-//	public void signUpNewUserTest(){
+//	@Order(3)
+//	public void createNotesTest_3() throws InterruptedException {
 //
+//		String title = "Test Title";
+//		String body = "Test Body";
 //
 //		driver.get("http://localhost:" + this.port + "/signup");
 //		SignUpPage signUpPage = new SignUpPage(driver);
 //		signUpPage.signUp(firstName,lastName,userName,password);
 //
+//
 //		driver.get("http://localhost:" + this.port + "/login");
 //		LoginPage loginPage = new LoginPage(driver);
 //		loginPage.login(userName,password);
 //
-//		// Login and check if user is able to access home page
-//		Assertions.assertEquals("Home", driver.getTitle());
-//
 //		driver.get("http://localhost:" + this.port + "/home");
+//		Thread.sleep(1000);
 //		HomePage homePage = new HomePage(driver);
-//		homePage.logout();
+//     	homePage.selectNotesTab();
 //
-//		//Logout and verify that the user is not able to access home page
+//		Thread.sleep(1000);
+//     	NotesPage notesPage = new NotesPage(driver);
+//     	notesPage.clickAddButton();
+//
+//		Thread.sleep(1000);
+//		notesPage.addNotes(title,body);
+//		notesPage.clickSubmitNotes();
+//		Thread.sleep(1000);
+//
+//		ResultPage resultPage = new ResultPage(driver);
+//		Assertions.assertTrue(resultPage.isSuccessMessageDisplayed());
+//
 //		driver.get("http://localhost:" + this.port + "/home");
-//		Assertions.assertEquals("Login",driver.getTitle());
+//		Thread.sleep(1000);
+//		homePage = new HomePage(driver);
+//		homePage.selectNotesTab();
 //
+//		notesPage = new NotesPage(driver);
+//		Assertions.assertTrue(notesPage.doesNotesExist(title));
 //	}
+
+	@Test
+	@Order(3)
+	public void createNotesTest_3() throws InterruptedException {
+
+		String title = "Test Title";
+		String body = "Test Body";
+
+		driver.get("http://localhost:" + this.port + "/signup");
+		SignUpPage signUpPage = new SignUpPage(driver);
+		signUpPage.signUp(firstName,lastName,userName,password);
+
+
+		driver.get("http://localhost:" + this.port + "/login");
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(userName,password);
+
+		driver.get("http://localhost:" + this.port + "/home");
+		Thread.sleep(1000);
+		HomePage homePage = new HomePage(driver);
+		homePage.selectNotesTab();
+
+		Thread.sleep(1000);
+		NotesPage notesPage = new NotesPage(driver);
+		notesPage.clickAddButton();
+
+		Thread.sleep(1000);
+		notesPage.addNotes(title,body);
+		notesPage.clickSubmitNotes();
+		Thread.sleep(1000);
+
+		ResultPage resultPage = new ResultPage(driver);
+		Assertions.assertTrue(resultPage.isSuccessMessageDisplayed());
+
+		driver.get("http://localhost:" + this.port + "/home");
+		Thread.sleep(1000);
+		homePage = new HomePage(driver);
+		homePage.selectNotesTab();
+
+		notesPage = new NotesPage(driver);
+		Assertions.assertTrue(notesPage.doesNotesExist(title));
+	}
 
 
 	@Test
-	public void createNotesTest() throws InterruptedException {
+	@Order(4)
+	public void updateNotesTest_4() throws InterruptedException {
+		String updatedTitle = "Updated Test Title";
+		String title = "Test Title";
+		String body = "Test Body";
 
+		driver.get("http://localhost:" + this.port + "/signup");
+		SignUpPage signUpPage = new SignUpPage(driver);
+		signUpPage.signUp(firstName,lastName,userName,password);
+
+
+		driver.get("http://localhost:" + this.port + "/login");
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(userName,password);
+
+		driver.get("http://localhost:" + this.port + "/home");
+		Thread.sleep(1000);
+		HomePage homePage = new HomePage(driver);
+		homePage.selectNotesTab();
+
+		Thread.sleep(1000);
+		NotesPage notesPage = new NotesPage(driver);
+		notesPage.clickAddButton();
+
+		Thread.sleep(1000);
+		notesPage.addNotes(title,body);
+		notesPage.clickSubmitNotes();
+		Thread.sleep(1000);
+
+		driver.get("http://localhost:" + this.port + "/home");
+		Thread.sleep(1000);
+		homePage = new HomePage(driver);
+
+		homePage.selectNotesTab();
+		Thread.sleep(1000);
+		notesPage.editNotes(updatedTitle);
+
+		driver.get("http://localhost:" + this.port + "/home");
+		Thread.sleep(1000);
+		homePage = new HomePage(driver);
+     	homePage.selectNotesTab();
+
+     	notesPage = new NotesPage(driver);
+     	Assertions.assertTrue(notesPage.doesNotesExist(updatedTitle));
+
+	}
+
+
+	@Test
+	@Order(5)
+	public void deleteNoteTest_5() throws InterruptedException {
 		String title = "Test Title";
 		String body = "Test Body";
 
@@ -125,16 +262,19 @@ class CloudStorageApplicationTests {
 		notesPage.clickSubmitNotes();
 		Thread.sleep(1000);
 
-		ResultPage resultPage = new ResultPage(driver);
-		Assertions.assertTrue(resultPage.isSuccessMessageDisplayed());
-
 		driver.get("http://localhost:" + this.port + "/home");
 		Thread.sleep(1000);
 		homePage = new HomePage(driver);
 		homePage.selectNotesTab();
+		Thread.sleep(1000);
 
 		notesPage = new NotesPage(driver);
-		Assertions.assertTrue(notesPage.doesNotesExist(title,body));
+		notesPage.deleteNote();
+		Thread.sleep(1000);
 
+		ResultPage resultPage = new ResultPage(driver);
+		Assertions.assertTrue(resultPage.isSuccessMessageDisplayed());
 	}
+
+
 }
--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/Pages/NotesPage.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/Pages/NotesPage.java
@@ -1,10 +1,13 @@
 package com.udacity.jwdnd.course1.cloudstorage.Pages;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -26,8 +29,11 @@ public class NotesPage {
     @FindBy(id = "note-description")
     private WebElement notesDescription;
 
+    WebDriver webDriver;
+
     public NotesPage(WebDriver driver) {
         PageFactory.initElements(driver,this);
+        this.webDriver = driver;
     }
 
     public void addNotes(String title,String desc){
@@ -43,8 +49,9 @@ public class NotesPage {
         submitNotes.click();
     }
 
-    public boolean doesNotesExist(String noteTitle,String noteBody){
+    public boolean doesNotesExist(String noteTitle){
         List<WebElement> notesTitleList = userTable.findElements(By.tagName("th"));
+
         Boolean created = false;
         for (int i=0; i < notesTitleList.size(); i++) {
             WebElement titleElement = notesTitleList.get(i);
@@ -54,6 +61,42 @@ public class NotesPage {
             }
         }
         return created;
+    }
+
+    public void editNotes(String newNoteTitle){
+        WebDriverWait wait = new WebDriverWait (webDriver, 30);
+        WebElement notesTable = userTable;
+        List<WebElement> notesList = notesTable.findElements(By.tagName("td"));
+        WebElement editElement = null;
+        for (int i = 0; i < notesList.size(); i++) {
+            WebElement element = notesList.get(i);
+            editElement = element.findElement(By.name("edit"));
+            if (editElement != null){
+                break;
+            }
+        }
+        wait.until(ExpectedConditions.elementToBeClickable(editElement)).click();
+        WebElement notetitle = webDriver.findElement(By.id("note-title"));
+        wait.until(ExpectedConditions.elementToBeClickable(notetitle));
+        notetitle.clear();
+        notetitle.sendKeys(newNoteTitle);
+        submitNotes.click();
+
+    }
+
+    public void deleteNote(){
+        WebDriverWait wait = new WebDriverWait (webDriver, 30);
+        WebElement notesTable = userTable;
+        List<WebElement> notesList = notesTable.findElements(By.tagName("td"));
+        WebElement deleteElement = null;
+        for (int i = 0; i < notesList.size(); i++) {
+            WebElement element = notesList.get(i);
+            deleteElement = element.findElement(By.name("delete"));
+            if (deleteElement != null){
+                break;
+            }
+        }
+        wait.until(ExpectedConditions.elementToBeClickable(deleteElement)).click();
     }
 
 
